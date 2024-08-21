cd0b49e087b256f930c5dcb17cb8d503b1ae6f6f
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-08-21 16:26:30 +0300

    add routing to slides screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomeScreen } from "./screens/homeScreen";
+import { SlideScreen } from "./screens/slideScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/presentation/:title/slides" element={<SlideScreen />} />
       </Routes>
     </Router>
   );
