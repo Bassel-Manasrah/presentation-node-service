8863b6c7b0ae6b08e7e87933571d1de02551418c
Bassel Manasrah <bassl.manasrah@gmail.com>
2024-08-21 15:44:29 +0300

    add text input component

---
+++ frontend/src/common/components/textInput/index.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import styles from "./style.module.css";
+
+interface textInputProps {
+  value: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  multiLine?: boolean;
+  placeholder?: string;
+  rows?: number;
+}
+
+export const TextInput: FC<textInputProps> = ({
+  value,
+  onChange,
+  multiLine = false,
+  placeholder = "",
+  rows = 3,
+}) => {
+  const sharedProps = {
+    value,
+    onChange,
+    placeholder,
+    className: styles.input,
+  };
+
+  if (multiLine) {
+    return <textarea {...sharedProps} rows={rows} />;
+  }
+
+  return <input type="text" {...sharedProps} />;
+};
